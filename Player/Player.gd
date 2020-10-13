ae98776257b2331a760fcd64a2b4b1e19d376d2a
YM <thehousewhite@gmail.com>
2020-10-13 21:39:59 +0200

    animatedsprite

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,17 +2,52 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 const SPEED = 1000
+const GRAVITY = 150
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 3500
 
-var i = 0
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP) # engine automatically calculate motion*delta
+	
+	
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
+
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion) # engine automatically calculate
-	i = i+1
-	print(i)
-	
-	
+
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x < 0:
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walk")
+	elif motion.x > 0:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
+
+
+
+
+
+
+
