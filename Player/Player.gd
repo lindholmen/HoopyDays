987e7d2cedec50a9c195ad5f189fb358daea4ef1
YM <thehousewhite@gmail.com>
2020-10-17 23:30:46 +0200

    fixed onceiling issue, added Parallax background, create game over sceen

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ const SPEED = 1000
 const GRAVITY = 150
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 3000
+const WORLD_LIMIT = 3000
 
 signal animate
 
@@ -17,8 +18,14 @@ func _physics_process(delta):
 	
 	
 func apply_gravity():
+	$"../LabelTest".text = str(self.position[0])
+	
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if not is_on_floor():
 		motion.y += GRAVITY
+		if is_on_ceiling():
+			motion.y = 1
 	else:
 		motion.y = 0
 
@@ -39,6 +46,11 @@ func animate():
 	# Every frame will emite a signal called animate with a parametre called motion
 	emit_signal("animate", motion)
 
+
+func end_game():
+	#get_tree().quit()
+	get_tree().change_scene("res://Levels/GameOver.tscn")
+	
 
 
 
